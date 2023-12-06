edc1872821fed66c59ed60bc3c3013b96735a3e3
Nikita Nikolayuk <113298204+minimaledit@users.noreply.github.com>
2023-12-07 01:37:35 +0300

    add Authorization
    log in/off

---
+++ RestApp.Web/Pages/LoginModel.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Components;
+using RestApp.Web.Infrastructure;
+using System.ComponentModel.DataAnnotations;
+
+namespace RestApp.Web.Pages
+{
+    public class LoginModel : ComponentBase
+    {
+        [Inject] public ILocalStorageService LocalStorageService { get; set; }
+        [Inject] public NavigationManager NavigationManager { get; set; }
+        public LoginModel() 
+        {
+            LoginData = new LoginViewModel();
+        }
+        public LoginViewModel LoginData { get; set; }
+
+        protected async Task LoginAsync()
+        {
+            var token = new SecurityToken
+            {
+                AccessToken = LoginData.Password,
+                Email = LoginData.Email,
+                ExpiredAt = DateTime.UtcNow.AddDays(1)
+            };
+            await LocalStorageService.SetAsync(nameof(SecurityToken), token);
+
+            NavigationManager.NavigateTo("/", true);
+        }
+    }
+
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        public string Password { get; set; }
+    }
+
+    public class SecurityToken
+    {
+        public string Email { get; set; }
+        public string AccessToken { get; set; }
+        public DateTime ExpiredAt { get; set; }
+    }
+}
--- RestApp.Web/Program.cs
+++ RestApp.Web/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using RestApp.Web;
-
+using RestApp.Web.Infrastructure;
+using RestApp.Web.Pages;
+using System.Security.AccessControl;
+using System.Security.Claims;
+/*
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
@@ -9,3 +14,82 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
+*/
+namespace BlazorAuthentification1
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddOptions();
+            builder.Services.AddAuthorizationCore();
+
+            builder.Services.AddScoped<AuthenticationStateProvider, TokenAuthenticationStateProvider>();
+            builder.Services.AddScoped<ILocalStorageService, LocalStorageService>();
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+
+        }
+    }
+
+    internal class TokenAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        private readonly ILocalStorageService _localStorageService;
+        public TokenAuthenticationStateProvider(ILocalStorageService localStorageService) {
+            _localStorageService = localStorageService;
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        { 
+            AuthenticationState CreateAnonymous()
+            {
+                var anonymousIdentity = new ClaimsIdentity();
+                var anonymousPrincipal = new ClaimsPrincipal(anonymousIdentity);
+                return new AuthenticationState(anonymousPrincipal);
+            }
+
+            var token = await _localStorageService.GetAsync<SecurityToken>(nameof(SecurityToken));
+
+            if (token == null)
+            {
+                return CreateAnonymous();
+            }
+
+            if (string.IsNullOrEmpty(token.AccessToken) || token.ExpiredAt < DateTime.UtcNow) 
+            {
+                return CreateAnonymous();
+            }
+
+            // Create real user 
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Country,"Belarus"),
+                new Claim(ClaimTypes.Name, token.Email),
+                new Claim(ClaimTypes.Expired, token.ExpiredAt.ToLongDateString()),
+                new Claim(ClaimTypes.Role, "Administrator"),
+                new Claim(ClaimTypes.Role, "Manager"),
+                new Claim("Blazor1", "Eshkere")
+            };
+            
+            var identity = new ClaimsIdentity(claims, "Token");
+            var principal = new ClaimsPrincipal(identity);
+            return new AuthenticationState(principal);
+
+        }
+
+        public void MakeUserAnonymous()
+        {
+            _localStorageService.RemoveAsync(nameof(SecurityToken));
+
+            var anonymousIdentity = new ClaimsIdentity();
+            var anonymousPrincipal = new ClaimsPrincipal(anonymousIdentity);
+            var authState = Task.FromResult(new AuthenticationState(anonymousPrincipal));
+            NotifyAuthenticationStateChanged(authState);
+        }
+    }
+}
